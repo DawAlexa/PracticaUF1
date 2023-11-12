a8ce22c0b973998affb39d458ee5fa61c855755c
dawalexa <daw.alexa1@gmail.com>
2023-11-12 19:33:14 +0100

    Abrimos la carpeta y en la terminal ejecuto dotnet new console -o app

---
+++ app/Program.cs
@@ -0,0 +1,29 @@
+﻿// Creamos un programa que ayuden a los usuarios a calcular el IVA
+
+using System;
+
+namespace ivaCalculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programa para calcular iva segun cantidad.
+
+            
+            
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
